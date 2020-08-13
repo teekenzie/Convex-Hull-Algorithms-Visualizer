5e9817378b3ed0a27e31f69f263565dd43cf0ef2
teekenzie <teekenzieqx@gmail.com>
2020-08-12 17:27:18 -0700

    Added speed slider

--- userinterface/Visualizer.java
+++ userinterface/Visualizer.java
@@ -1,9 +1,11 @@
 package userinterface;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Objects;
 
 import algorithms.*;
@@ -20,13 +22,14 @@ public class Visualizer extends JFrame {
 
     JButton visualize, random, reset;
     JComboBox<String> options;
+    JSlider speed;
     AnimationArea animationArea;
     HashSet<Point> points;
     Timer timer;
 
     public Visualizer() {
         JPanel thePanel = new JPanel();
-        this.setSize(700, 700);
+        this.setSize(815, 640);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -36,29 +39,46 @@ public class Visualizer extends JFrame {
 
         visualize = new JButton("Visualize");
         thePanel.add(visualize);
-        visualize.setBounds(300, 10, 100, 50);
+        visualize.setBounds(610, 240, 180, 75);
+        visualize.setFont(new Font("TimesRoman", Font.BOLD, 30));
         visualize.addActionListener(lForButton);
 
         String[] choices = { "Chan's Algorithm","Marriage before Conquest", "Incremental", "Monotone Chain", "Divide and Conquer" ,
                 "Quick Hull" , "Graham Scan",  "Jarvis March"};
         options = new JComboBox<>(choices);
         thePanel.add(options);
-        options.setBounds(105, 10, 180, 50);
+        options.setBounds(610, 110, 180, 50);
 
         random = new JButton("Random Points");
         thePanel.add(random);
-        random.setBounds(415,10,150,50);
+        random.setBounds(625,330,150,40);
+        // random.setBounds(610,330,180,40);
         random.addActionListener(lForButton);
 
         reset = new JButton("Clear");
         thePanel.add(reset);
-        reset.setBounds(15, 10, 75, 50);
+        reset.setBounds(663, 385, 70, 40);
+        // reset.setBounds(610, 385, 180, 40);
         reset.addActionListener(lForButton);
+
+        speed = new JSlider(JSlider.HORIZONTAL, 0, 200, 100 );
+        thePanel.add(speed);
+        speed.setBounds(610, 180, 180, 45);
+        speed.setMajorTickSpacing(50);
+        speed.setPaintTicks(true);
+        speed.setSnapToTicks(true);
+
+        Hashtable<Integer, JLabel> labelTable = new Hashtable<>();
+        labelTable.put( 0, new JLabel("Slow") );
+        labelTable.put( 100, new JLabel("Normal") );
+        labelTable.put( 200, new JLabel("Fast") );
+        speed.setLabelTable( labelTable );
+        speed.setPaintLabels(true);
 
         points = new HashSet<>();
         animationArea = new AnimationArea(points);
         thePanel.add(animationArea);
-        animationArea.setBounds(-10, 65, animationArea.getCanvasSize(), animationArea.getCanvasSize());
+        animationArea.setBounds(0, 0, animationArea.getCanvasSize(), animationArea.getCanvasSize());
 
         this.add(thePanel);
         this.setVisible(true);
@@ -67,17 +87,21 @@ public class Visualizer extends JFrame {
     private void errorMessage(String message) { JOptionPane.showMessageDialog(this, message, "ERROR", JOptionPane.ERROR_MESSAGE); }
 
     private class ListenForButton implements ActionListener {
+        int speedVal;
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == reset) {
                 points = new HashSet<>();
                 animationArea.reset(points);
-                reset.setBounds(15, 10, 75, 50);
+                reset.setBounds(663, 385, 70, 40);
+                reset.setFont(new Font(null, Font.BOLD, 12));
                 visualize.setVisible(true);
                 options.setVisible(true);
                 random.setVisible(true);
+                speed.setVisible(true);
                 visualize.setEnabled(true);
                 options.setEnabled(true);
                 random.setEnabled(true);
+                speed.setEnabled(true);
                 reset.setText("Clear");
             }
             else if (e.getSource() == random) {
@@ -86,6 +110,7 @@ public class Visualizer extends JFrame {
             }
             else if( e.getSource()== visualize)
             {
+                speedVal = 200 - speed.getValue();
                 if (points.size() < 3) {
                     errorMessage("At least 3 points is needed");
                     return;
@@ -103,6 +128,7 @@ public class Visualizer extends JFrame {
                 options.setEnabled(false);
                 random.setEnabled(false);
                 reset.setEnabled(false);
+                speed.setEnabled(false);
 
                 switch (Objects.requireNonNull(userChoice)) {
                     case "Marriage before Conquest":
@@ -132,7 +158,7 @@ public class Visualizer extends JFrame {
                     default:
                         throw new IllegalStateException("Unexpected value: " + Objects.requireNonNull(userChoice));
                 }
-                timer = new Timer(algorithm.getTime(), actionEvent -> runAnimation(algorithm));
+                timer = new Timer((int) (algorithm.getTime()*(speedVal/100.0)), actionEvent -> runAnimation(algorithm));
                 timer.start();
             }
         }
@@ -148,11 +174,13 @@ public class Visualizer extends JFrame {
         {
             timer.stop();
             reset.setText("Reset");
-            reset.setBounds(35, 10, 510, 50);
+            reset.setBounds(610, 215, 180, 100);
             reset.setEnabled(true);
             visualize.setVisible(false);
             options.setVisible(false);
             random.setVisible(false);
+            speed.setVisible(false);
+            reset.setFont(new Font("TimesRoman", Font.PLAIN, 40));
         }
     }
 }
